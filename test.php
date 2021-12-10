86e03bfec65947f98e1ee542dc9129854f910b6c
find-404 <firdiansyaindrianto@gmail.com>
2021-12-10 23:40:52 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php 
+$hasil = 'https://streaming.useetv.com/dass/134/thrill_dash/';
+$tambahan = '0';
+$bandwidth = 'b1004100';
+$key = 'enc=222sg466ggh1a490l1v4408bk1';
+$time = intval(time() / 10.0 ) - 163876555;
+?>
